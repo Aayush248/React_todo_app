5ee415ab40ac14e9e40a175c9af8de6c23091349
Aayush248 <aayushthapa1@hotmail.com>
2024-05-05 21:19:35 +0545

    Can strikethrough the complete tasks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,10 +3,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
- const Todo = ({task}) => {
+ const Todo = ({task, toggleComplete}) => {
   return (
     <div className='Todo'>
-        <p>{task.task}</p>
+        <p onClick={() => toggleComplete(task.id)}className={`${task.completed ? 'completed' : ''}`}>{task.task}</p>
         <div>
             <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
             <FontAwesomeIcon className="delete-icon" icon={faTrash} />
@@ -16,3 +16,6 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons'
 }
 
 export default Todo;
+
+
+
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -12,11 +12,17 @@ const TodoWrapper = () => {
       setTodos([...todos,{id: uuidv4(), task: todo, completed: false, isEditing: false}])
       console.log(todos)
     }
+
+    const toggleComplete = id =>{
+      setTodos(todos.map(todo => todo.id == id ? {...todo, completed: !todo.completed} : todo))
+    }
+
   return (
     <div className='TodoWrapper'>
       <TodoForm addTodo={addTodos}/>
       {todos.map((todo, index)=>(
-        <Todo task={todo} key={index}/>
+        <Todo task={todo} key={index}
+        toggleComplete={toggleComplete}/>
       ))}
     </div>
   )

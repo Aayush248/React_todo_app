69a80a3c193c8d98a56c85b0e7cde09d0dfbaa01
Aayush248 <aayushthapa1@hotmail.com>
2024-05-05 13:51:59 +0545

    can add tasks for now

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-const Todo = () => {
+ const Todo = ({task}) => {
   return (
-    <div>
-      
+    <div className='Todo'>
+        <p>{task.task}</p>
+        <div>
+            <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} />
+            <FontAwesomeIcon className="delete-icon" icon={faTrash} />
+        </div>
     </div>
   )
 }
 
-export default Todo
+export default Todo;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const TodoForm = () => {
+const TodoForm = ({addTodo}) => {
+  const[value, setValue] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(value);
+
+    setValue('')
+  }
   return (
-    <form classname='TodoForm'>
-      <input type="text" className='todo-input' placeholder='task'/>
-      <button type="submit" className='todo-btn'></button>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type="text" className='todo-input' value={value} placeholder='Enter a Task' onChange={(e) => setValue(e.target.value)}/>
+      <button type="submit" className='todo-btn'>Add Task</button>
     </form>
   )
 }
